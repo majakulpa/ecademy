dde70d033fca5a3e6c96fb92b5f621de4079aa00
majakulpa <majaweronikakulpa@gmail.com>
2020-06-05 00:41:53 +1000

    modify course model, add search query

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -2,8 +2,17 @@ const Course = require('./../models/courseModel')
 
 exports.getAllCourses = async (req, res) => {
   try {
-    const courses = await Course.find()
+    // build query
+    const queryObj = { ...req.query }
+    const excludedFields = ['page', 'sort', 'limit', 'fields']
+    excludedFields.forEach((el) => delete queryObj[el])
 
+    const query = await Course.find(queryObj)
+
+    // execute query
+    const courses = await query
+
+    // send response
     res.status(200).json({
       status: 'success',
       results: courses.length,
@@ -50,7 +59,7 @@ exports.createCourse = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent!',
+      message: err,
     })
   }
 }
--- models/courseModel.js
+++ models/courseModel.js
@@ -7,14 +7,45 @@ const courseSchema = new mongoose.Schema({
     required: [true, 'A course must have a name'],
     unique: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A course must have a duration'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  level: {
+    type: String,
+    required: [true, 'Please select level'],
   },
   price: {
     type: Number,
     required: [true, 'A course must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A course must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A course must have a cover image'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  courseContent: [String],
 })
 
 // model
